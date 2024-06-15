c8590bd8f43b5e46f9132d3b4c85c0caa3ea79de
Hyper Weavers <info@hyperweavers.com>
2024-06-15 15:25:21 +0530

    add pwa

--- apps/pangu/src/app/app.config.ts
+++ apps/pangu/src/app/app.config.ts
@@ -1,7 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
