fbd7a1167c21de2ef159066dfda3c96bc44e7d8e
Hyper Weavers <info@hyperweavers.com>
2024-12-21 20:42:01 +0530

    Modify home page

--- apps/kanippaan/src/app/pages/home/home.page.ts
+++ apps/kanippaan/src/app/pages/home/home.page.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+import { Constants } from '../../constants';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, RouterLink],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {}
+export class HomePage {
+  public readonly Routes = Constants.routes;
+}
