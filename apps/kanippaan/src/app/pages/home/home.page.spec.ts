80f45a38785adde48e09c3feec350b8dc7958ab6
Hyper Weavers <info@hyperweavers.com>
2024-12-21 22:05:28 +0530

    fix unit tests

--- apps/kanippaan/src/app/pages/home/home.page.spec.ts
+++ apps/kanippaan/src/app/pages/home/home.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { HomePage } from './home.page';
 
@@ -9,6 +10,7 @@ describe('HomePage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomePage],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
