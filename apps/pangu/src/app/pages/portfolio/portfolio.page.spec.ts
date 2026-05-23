530596f897a5608a07dfeaf64f95cb109b9062cb
Hyper Weavers <info@hyperweavers.com>
2026-05-23 19:27:33 +0000

    fix ut

--- apps/pangu/src/app/pages/portfolio/portfolio.page.spec.ts
+++ apps/pangu/src/app/pages/portfolio/portfolio.page.spec.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { PortfolioPage } from './portfolio.page';
 
@@ -10,7 +11,7 @@ describe('PortfolioPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PortfolioPage],
-      providers: [provideHttpClient()],
+      providers: [provideHttpClient(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PortfolioPage);
