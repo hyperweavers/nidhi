2d19519496e9265b3760a0d55411c8a9e6b4587d
Hyper Weavers <info@hyperweavers.com>
2026-08-08 19:02:37 +0530

    fix formatting issue

--- apps/palan/src/app/services/core/market.service.ts
+++ apps/palan/src/app/services/core/market.service.ts
@@ -272,9 +272,12 @@ export class MarketService {
                 return s === ChartResponseStatus.OK && t && t.length > 0
                   ? ([...new Set(t)].map((date, i) => {
                       // Multiply by 1000 to convert seconds to milliseconds
-                      const time = new Date(date * 1000).toLocaleDateString('en-CA', {
-                        timeZone: 'Asia/Kolkata',
-                      });
+                      const time = new Date(date * 1000).toLocaleDateString(
+                        "en-CA",
+                        {
+                          timeZone: "Asia/Kolkata",
+                        },
+                      );
                       const previousDayClose = i > 0 ? c[i - 1] : undefined;
                       const todayClose = c[i];
 
