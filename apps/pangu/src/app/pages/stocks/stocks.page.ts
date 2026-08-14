c970e40b3439e03d8dddad7175963b636421202e
Hyper Weavers <info@hyperweavers.com>
2026-08-14 23:53:33 +0530

    fix(pangu): check quantity before showing portfolio stock icon

--- apps/pangu/src/app/pages/stocks/stocks.page.ts
+++ apps/pangu/src/app/pages/stocks/stocks.page.ts
@@ -108,7 +108,7 @@ export class StocksPage implements OnDestroy {
           map(
             (portfolio) =>
               !!id &&
-              portfolio.holdings.some((h) => h.vendorCode.etm.primary === id),
+              portfolio.holdings.some((h) => h.vendorCode.etm.primary === id && h.quantity > 0),
           ),
         ),
       ),
